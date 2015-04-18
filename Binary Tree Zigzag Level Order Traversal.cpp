aa6c3791bf1eaa0f53fbd4c1470e6f2817cf96fd
Yuanxiang Ying <yingyuanxiang34@sina.com>
2015-04-18 09:14:18 +0800

    update

---
+++ Binary Tree Zigzag Level Order Traversal.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <stack>
+#include <cstdio>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<vector<int> > zigzagLevelOrder(TreeNode *root) {
+		vector<vector<int> > ans;
+        stack<TreeNode*> s[2];
+		int l = 0;
+
+		if(root)
+			s[l].push(root);
+
+		while (!s[l].empty()){
+			vector<int> row;
+			while(!s[l].empty()){
+				TreeNode* t = s[l].top();
+				s[l].pop();
+				row.push_back(t->val);
+				if(l == 0){
+					if(t->left)
+						s[1].push(t->left);
+					if(t->right)
+						s[1].push(t->right);
+				}else{
+					if(t->right)
+						s[0].push(t->right);
+					if(t->left)
+						s[0].push(t->left);
+				}
+			}
+			ans.push_back(row);
+			l = (l+1) % 2;
+		}
+
+		return ans;
+    }
+};
